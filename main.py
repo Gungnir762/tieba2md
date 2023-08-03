75c78558d0dc02c19161ede99ea6da4c1e223339
龙雀 <83220033+Gungnir762@users.noreply.github.com>
2023-08-03 16:53:23 +0800

    update

--- main.py
+++ main.py
@@ -34,13 +34,29 @@ def get_title_and_sumpage(session, post_url):
 
 
 # 获取该页面内的所有楼层内容
-def get_text(soup) -> list:
+def get_text(soup, img_dir) -> list:
     items_with_class = soup.find_all(class_="d_post_content j_d_post_content")
     text_list = []
     # print(items_with_class)
     for item in items_with_class:
         # 去除首尾空格
-        text_list.append(item.get_text().strip() + "\n")
+        text = item.get_text().strip() + "\n"
+        # 保存可能存在的图片
+        soup_img = BeautifulSoup(str(item), "lxml")
+        try:
+            img_src = soup_img.find('img')['src']
+            # print(img_src)
+            img_name = (img_src.split("/")[-1]).split("?")[0]
+            # print(img_name)
+            img_path = os.path.join(img_dir, img_name)
+            img_text = f"![{img_name.split('.')[0]}]({img_path})"
+            text += img_text + "\n"
+            with open(img_path, "wb") as f:
+                f.write(requests.get(img_src).content)
+        except:
+            # print("没有图片")
+            pass
+        text_list.append(text)
     return text_list
 
 
@@ -59,6 +75,8 @@ def get_floor_info(soup):
 if __name__ == '__main__':
     tid = "8528911427"
     post_url = rf"https://tieba.baidu.com/p/{tid}?see_lz=1"
+
+    img_dir = "./images"
     session = requests.session()
     cookies_dict = getcookies_decode_to_dict()
     session.cookies.update(cookies_dict)
@@ -69,8 +87,10 @@ if __name__ == '__main__':
     # 创建以标题为名的文件夹
     if not os.path.exists(title):
         os.mkdir(title)
-
     os.chdir(title)
+    if not os.path.exists(img_dir):
+        os.mkdir(img_dir)
+
     text_list = []
 
     # eg.[['1楼', '2023-07-31 02:10'], ['2楼', '2023-07-31 02:12']]
@@ -83,20 +103,21 @@ if __name__ == '__main__':
         response = session.get(page_url, headers=headers)
         soup = BeautifulSoup(response.text, 'lxml')
         floor_info_list.append(get_floor_info(soup))
-        text_list.append(get_text(soup))
-        # print(text_list)
-        break
+        text_list.append(get_text(soup, img_dir))
 
-    # print(len(floor_info_list[0]), len(text_list[0]))
-    # print(floor_info_list[0])
-    # print(text_list[0])
+    # with open('floor_info_list.json', 'w', encoding='utf-8') as f:
+    #     f.write(json.dumps(floor_info_list, ensure_ascii=False))
+    #
+    # with open('text_list.json', 'w', encoding='utf-8') as f:
+    #     f.write(json.dumps(text_list, ensure_ascii=False))
 
+    # 共8页就是range(7)
     for i in range(sumpage):
-        for floor in zip(floor_info_list[i], text_list[i]):
-            content_str += f'##### <span id="{floor[i][0]}">{floor[i][0]} 发表于{floor[i][1]}</span>\n'
-            content_str += floor[1] + "\n"
-        break
+        print(f"正在写入第{i + 1}页")
+        for floor, text in zip(floor_info_list[i], text_list[i]):
+            content_str += f'##### <span id="{floor[0]}">{floor[0]} 发表于{floor[1]}</span>\n'
+            content_str += text + "\n"
 
-    print(content_str)
+    # print(content_str)
     with open('content.md', 'w', encoding='utf-8') as f:
         f.write(content_str)
